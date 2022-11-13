8642ed0ae2f1ffd3b9538194bb7aeba3dab6a15d
IvanTratsevski <tratsevskiyivan@gmail.com>
2022-11-13 12:03:02 +0300

    Main base

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 const routes: Routes = [
-  { path: '', component: HeaderComponent}
+  { path: '', component: HeaderComponent},
 ]
 
 @NgModule({
